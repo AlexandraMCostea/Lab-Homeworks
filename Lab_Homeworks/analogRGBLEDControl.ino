1086c560b02c6c042fd3e782faee047e26196dde
AlexandraMCostea <38553471+AlexandraMCostea@users.noreply.github.com>
2019-11-16 12:16:28 +0200

    Create analogRGBLEDControl.ino

---
+++ Lab_Homeworks/analogRGBLEDControl.ino
@@ -0,0 +1,38 @@
+const int redPin = 9;
+const int greenPin = 10;
+const int bluePin = 11;
+const int redIn = 0; 
+const int greenIn = 1; 
+const int blueIn = 2; 
+
+int redVal; 
+int greenVal; 
+int blueVal; 
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(redPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  
+  pinMode(redIn, INPUT);
+  pinMode(greenIn, INPUT);
+  pinMode(blueIn,INPUT);
+}
+
+void loop() { 
+redVal = analogRead(redIn); 
+greenVal = analogRead(greenIn); 
+blueVal = analogRead(blueIn); 
+redVal = map(redVal, 0, 1023, 0, 255); 
+greenVal = map(greenVal, 0, 1023, 0, 255); 
+blueVal = map(blueVal, 0, 1023, 0, 255); 
+setColor(redVal, greenVal, blueVal);
+}
+
+void setColor(int redVal, int greenVal, int blueVal){
+analogWrite(redPin, redVal); 
+analogWrite(greenPin, greenVal); 
+analogWrite(bluePin, blueVal); 
+}
